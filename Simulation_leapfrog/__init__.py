ac41845b65e3d2451dff19d75fef238ce2f534b4
olivier1 <olivier.chalifour1@ulaval.ca>
2020-04-13 11:13:29 -0400

    Create __init__.py

---
+++ Simulation_leapfrog/__init__.py
@@ -0,0 +1,7 @@
+import numpy as np
+systeme_solaire_symplifier={'corps':['Soleil','Jupiter','Saturne','Uranus'],
+                            'position':np.array([np.array([0,0,0]),np.array([5.20,0,0]),np.array([9.54,0,0]),np.array([19.18,0,0])]),
+                            'vitesse':np.array([np.array([0,0,0]),np.array([5.20,0,0]),np.array([9.54,0,0]),np.array([19.18,0,0])]),
+                            'masse':np.array([0,0,0,0])}
+if __name__ == '__main__':
+    pass
