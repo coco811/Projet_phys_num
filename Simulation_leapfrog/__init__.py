50f0159b4fbe2fe4bfd6f5a0821cb790968b0263
olivier1 <olivier.chalifour1@ulaval.ca>
2020-04-16 14:28:09 -0400

    Update __init__.py

--- Simulation_leapfrog/__init__.py
+++ Simulation_leapfrog/__init__.py
@@ -1,7 +1,8 @@
 import numpy as np
+from Simulation_leapfrog import methode_saute_mouton as sp
 systeme_solaire_symplifier={'corps':['Soleil','Jupiter','Saturne','Uranus'],
                             'position':np.array([np.array([0,0,0]),np.array([5.20,0,0]),np.array([9.54,0,0]),np.array([19.18,0,0])]),
                             'vitesse':np.array([np.array([0,0,0]),np.array([5.20,0,0]),np.array([9.54,0,0]),np.array([19.18,0,0])]),
                             'masse':np.array([0,0,0,0])}
 if __name__ == '__main__':
-    pass
+    sp.saute_mouton(systeme_solaire_symplifier, 0, 10,'Méthode saute mouton pour le sysème solaire').__call__()
