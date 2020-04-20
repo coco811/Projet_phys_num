f7583295ce2d48ff572b6d11d9f1b6fc0a3dbee9
olivier1 <olivier.chalifour1@ulaval.ca>
2020-04-20 14:10:31 -0400

    Update __init__.py

--- Simulation_N_corps/__init__.py
+++ Simulation_N_corps/__init__.py
@@ -1,8 +1,5 @@
+from Simulation_N_corps import Donnée
 from Simulation_N_corps import simulation_1 as sm1
-import math
-import random
-import matplotlib.pyplot as plot
-from mpl_toolkits.mplot3d import Axes3D
 
 sun = {"position": sm1.point(0, 0, 0), "masse": 2e30, "vitesse": sm1.point(0, 0, 0)}
 mercury = {"position": sm1.point(0, 5.7e10, 0), "masse": 3.285e23, "vitesse": sm1.point(47000, 0, 0)}
@@ -27,8 +24,26 @@ if __name__ == "__main__":
         sm1.corps(position=uranus["position"], masse=uranus["masse"], vitesse=uranus["vitesse"], nom="Uranus "),
         sm1.corps(position=neptune["position"], masse=neptune["masse"], vitesse=neptune["vitesse"], nom="Neptune ")
     ]
+    mouvement_complet = sm1.run_simulation(corps_simulation_complete_systeme, pas_temps=10000, nombre_de_pas=350000,
+                                           frequence=5000)
+    # print(mouvement)
+    # sm1.Graphique_plusieurs_corps(mouvement,"Graphique de l'orbite d'Uranus avec l'influence de Neptune",6,7)
+    # sm1.Graphique_plusieurs_corps(mouvement, "Graphique de l'orbite d'Uranus et de Neptune",6,8, outfile=None)
 
-    mouvement= sm1.run_simulation(corps_simulation_complete_systeme, pas_temps=10000, nombre_de_pas=200000, frequence=5000)
-    # sm1.Graphique(motions, outfile=None)
-    sm1.Graphique_uranus_seul(mouvement,"Graphique de l'orbite d'Uranus avec l'influence de Neptune",outfile=None)
-    sm1.Graphique_uranus_neptune(mouvement, "Graphique de l'orbite d'Uranus et de Neptune avec l'influence de Neptune", outfile=None)
+    # systeme solaire sans neptune
+    corps_simulation_sans_Neptune = [
+        sm1.corps(position=sun["position"], masse=sun["masse"], vitesse=sun["vitesse"], nom="Sun"),
+        sm1.corps(position=earth["position"], masse=earth["masse"], vitesse=earth["vitesse"], nom="Earth"),
+        sm1.corps(position=mars["position"], masse=mars["masse"], vitesse=mars["vitesse"], nom="Mars"),
+        sm1.corps(position=venus["position"], masse=venus["masse"], vitesse=venus["vitesse"], nom="Venus"),
+        sm1.corps(position=jupiter["position"], masse=jupiter["masse"], vitesse=jupiter["vitesse"], nom="Jupiter"),
+        sm1.corps(position=saturn["position"], masse=saturn["masse"], vitesse=saturn["vitesse"], nom="Saturn"),
+        sm1.corps(position=uranus["position"], masse=uranus["masse"], vitesse=uranus["vitesse"], nom="Uranus "),
+    ]
+    #   comparaison reference et simulation Uranus
+    nom_fichier = 'horizons_results-3.txt'
+    nom_planete = 'Réference'
+    mouvement_ref = Donnée.extraire_donne(nom_fichier, nom_planete)
+    mouvement_complet[6]['nom'] = 'Simulation '
+    mouvement_ref.append(mouvement_complet[6])
+    sm1.Graphique(mouvement_ref, "Graphique de l'orbite d'Uranus avec l'influence de Neptune", outfile=None)
