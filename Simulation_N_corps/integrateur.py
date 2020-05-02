2d59a23679a4c66431099cf5bfeacc73976fb30d
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-02 12:44:15 -0400

    Update integrateur.py

--- Simulation_N_corps/integrateur.py
+++ Simulation_N_corps/integrateur.py
@@ -1,17 +1,18 @@
 import math
+
 from Simulation_N_corps import code_simulation as code
 
 
 class euler:
-    def __init__(self,corps,pas_temps=1):
+    def __init__(self, corps, pas_temps=1):
         self.pas_temps = pas_temps
         self.corps = corps
 
     def calcul_position(self):
         for corps_obs in self.corps:
-            corps_obs.position.x += corps_obs.vitesse.x * self.pas_temps
-            corps_obs.position.y += corps_obs.vitesse.y * self.pas_temps
-            corps_obs.position.z += corps_obs.vitesse.z * self.pas_temps
+            corps_obs.position.x += corps_obs.vitesse.x * self.pas_temps / 2
+            corps_obs.position.y += corps_obs.vitesse.y * self.pas_temps / 2
+            corps_obs.position.z += corps_obs.vitesse.z * self.pas_temps / 2
 
     def calcul_vitesse(self):
         for indice_corps, corps_obs in enumerate(self.corps):
@@ -27,8 +28,8 @@ class euler:
         for index, corps_exterieur in enumerate(self.corps):
             if index != indice_corps:
                 r = (corps_obs.position.x - corps_exterieur.position.x) ** 2 + (
-                            corps_obs.position.y - corps_exterieur.position.y) ** 2 + (
-                                corps_obs.position.z - corps_exterieur.position.z) ** 2
+                        corps_obs.position.y - corps_exterieur.position.y) ** 2 + (
+                            corps_obs.position.z - corps_exterieur.position.z) ** 2
                 r = math.sqrt(r)
                 tmp = G_const * corps_exterieur.masse / r ** 3
                 acceleration.x += tmp * (corps_exterieur.position.x - corps_obs.position.x)
@@ -38,6 +39,6 @@ class euler:
         return acceleration
 
     def calcul_pas_graviter(self):
+        self.calcul_position()
         self.calcul_vitesse()
         self.calcul_position()
-
