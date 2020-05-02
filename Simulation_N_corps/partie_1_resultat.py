0b8fe5376d60e46f08c28ac499c71de39c810617
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-02 15:20:57 -0400

    Update partie_1_resultat.py

--- Simulation_N_corps/partie_1_resultat.py
+++ Simulation_N_corps/partie_1_resultat.py
@@ -19,7 +19,7 @@ saturn = {"position": sm1.point(0, 1.4e12, 0), "masse": 5.7e26, "vitesse": sm1.p
 uranus = {"position": sm1.point(0, 2.8e12, 0), "masse": 8.7e25, "vitesse": sm1.point(6835, 0, 0)}
 neptune = {"position": sm1.point(0, 4.5e12, 0), "masse": 1e26, "vitesse": sm1.point(5477, 0, 0)}
 pluto = {"position": sm1.point(0, 3.7e12, 0), "masse": 1.3e22, "vitesse": sm1.point(4748, 0, 0)}
-satelite = {"position": sm1.point(0, 1.0e10, 0), "masse": 1e23, "vitesse": sm1.point(3e4, 0, 0)}
+
 
 if __name__ == "__main__":
     "  Simulation  "
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     # with open('mouvement_avec_neptune.p', 'rb') as fp:
     #     mouvement_complet_avec_Neptune_data = pickle.load(fp)
     #
-    # sm1.Graphique_plusieurs_corps(mouvement_complet_avec_Neptune_data[0],"Simulation de l'orbite des planètes géantes",5,9,outfile="Simulation de l'orbite des planètes géantes")
+    # sm1.Graphique_plusieurs_corps(mouvement_complet_avec_Neptune[0],"Simulation de l'orbite des planètes géantes",5,9,outfile=None)
 
     "   systeme solaire sans neptune  "
 
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         sm1.corps(position=uranus["position"], masse=uranus["masse"], vitesse=uranus["vitesse"], nom="Uranus "),
     ]
     # integration = inte.euler(corps_simulation_sans_Neptune, pas_temps=2 * 86400)
-    # mouvement_complet_sans_Neptune = sm1.run_simulation(integration, nombre_de_pas=(165 * 365), frequence=1)
+    # mouvement_complet_sans_Neptune = sm1.run_simulation(integration, nombre_de_pas=(165 * 365/2), frequence=1)
 
     # with open('mouvement_sans_neptune.p', 'wb') as fp:
     #     pickle.dump( mouvement_complet_sans_Neptune , fp, protocol=pickle.HIGHEST_PROTOCOL)
@@ -84,12 +84,12 @@ if __name__ == "__main__":
 
     " comparaison avec et sans Neptune de l'orbite d'Uranus"
 
-    # with open('mouvement_sans_neptune.p', 'rb') as fp:
-    #     mouvement_complet_sans_Neptune_data = pickle.load(fp)
-    #
-    # with open('mouvement_avec_neptune.p', 'rb') as fp:
-    #     mouvement_complet_avec_Neptune_data = pickle.load(fp)
-    #
+    with open('mouvement_sans_neptune.p', 'rb') as fp:
+        mouvement_complet_sans_Neptune_data = pickle.load(fp)
+
+    with open('mouvement_avec_neptune.p', 'rb') as fp:
+        mouvement_complet_avec_Neptune_data = pickle.load(fp)
+
     # mouvement_pour_graph=[]
     # mvt=[]
     # mouvement_complet_avec = mouvement_complet_avec_Neptune_data[0]
@@ -98,22 +98,22 @@ if __name__ == "__main__":
     # mouvement_complet_sans[7]['nom'] = 'Simulation\n sans Neptune '
     # mouvement_pour_graph.append([mouvement_complet_avec[0],mouvement_complet_avec[7]])
     # mouvement_pour_graph.append([mouvement_complet_sans[0], mouvement_complet_sans[7]])
-
+    #
     # Graph_2.Graphique_comparaison_avec_sans( mouvement_pour_graph,
     #               "Graphique de comparaison avec et sans l'influence de Neptune\n sur l'orbite d'Uranus",
     #               outfile='avec_sans_3d')
-    # Graph_2.graph2d_compa(mouvement_pour_graph,"Graphique de comparaison avec la référence \n de l'orbite d'Uranus avec l'influence de Neptune ",outfile='avec_sans_3d')
+    # Graph_2.graph2d_compa(mouvement_pour_graph,"Graphique de comparaison avec la référence \n de l'orbite d'Uranus avec l'influence de Neptune ",outfile=None)
 
     "  Comparaison reference et simulation Uranus Avec neptune  "
 
-    # mouvement_complet = mouvement_complet_avec_Neptune_data [0]
-    # nom_fichier = 'horizons_results-3.txt'
-    # nom_planete = 'Réference'
-    # mouvement_ref = extraction_donnees.extraire_donne(nom_fichier, nom_planete)
-    # mouvement_complet[7]['nom'] = 'Simulation\n avec Neptune '
-    # mouvement_ref.append(mouvement_complet)
+    mouvement_complet = mouvement_complet_avec_Neptune_data [0]
+    nom_fichier = 'horizons_results-3.txt'
+    nom_planete = 'Réference'
+    mouvement_ref = extraction_donnees.extraire_donne(nom_fichier, nom_planete)
+    mouvement_complet[7]['nom'] = 'Simulation\n avec Neptune '
+    mouvement_ref.append(mouvement_complet)
     # Graph_2.graph3d_ref(mouvement_ref, "Graphique de comparaison avec la référence \n de l'orbite d'Uranus avec l'influence de Neptune ", outfile='ref_Avec_3d')
-    # Graph_2.graph2d_ref(mouvement_ref,"Graphique de comparaison avec la référence \n de l'orbite d'Uranus avec l'influence de Neptune ",outfile='ref_Avec_2d')
+    Graph_2.graph2d_ref(mouvement_ref,"Graphique de comparaison avec la référence \n de l'orbite d'Uranus avec l'influence de Neptune ",outfile=None)
 
     "comparaison reference et simulation Uranus sans neptune"
 
