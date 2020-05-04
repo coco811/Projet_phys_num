dcaaf989738c67ef1a4523b15c87da3f705ffdc6
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-03 20:16:37 -0400

    Update partie_2_variation_para_resultat.py

--- Simulation_N_corps/partie_2_variation_para_resultat.py
+++ Simulation_N_corps/partie_2_variation_para_resultat.py
@@ -6,6 +6,8 @@ except ImportError:  # python 3.x
     import pickle
 from Simulation_N_corps import integrateur as inte
 import numpy as np
+from Simulation_N_corps import Graph_2
+
 vrai_sun = {"position": sm1.point(0, 0, 0), "masse": 2e30, "vitesse": sm1.point(0, 0, 1700)}
 sun = {"position": sm1.point(0, 0, 0), "masse": 2e30, "vitesse": sm1.point(0, 0, 0)}
 mercury = {"position": sm1.point(0, 5.7e10, 0), "masse": 3.285e23, "vitesse": sm1.point(47000, 0, 0)}
@@ -17,9 +19,6 @@ saturn = {"position": sm1.point(0, 1.4e12, 0), "masse": 5.7e26, "vitesse": sm1.p
 uranus = {"position": sm1.point(0, 2.8e12, 0), "masse": 8.7e25, "vitesse": sm1.point(7135, 0, 0)}
 neptune = {"position": sm1.point(0, 4.5e12, 0), "masse": 1e26, "vitesse": sm1.point(5477, 0, 0)}
 pluto = {"position": sm1.point(0, 5.9e12, 0), "masse": 1.3e22, "vitesse": sm1.point(4748, 1700, 1000)}
-
-# test planète rogue
-rogue_1 = {"position": sm1.point(3e12, 3.7e10, 0), "masse": 1e28, "vitesse": sm1.point(-5300, 1300, 300)}
 
 # Planéte 9
 #  situé a environ (350-800 ua) et masse environ (10mt)
@@ -60,7 +59,7 @@ if __name__ == '__main__':
     "Simulation planete X(9) "
 
     corps_simulation_planete_9_min = [
-        sm1.corps(position=sun["position"], masse=sun["masse"], vitesse=sun["vitesse"], nom="Sun"),
+        sm1.corps(position=sun["position"], masse=sun["masse"], vitesse=sun["vitesse"], nom="Soleil"),
         sm1.corps(position=mercury["position"], masse=mercury["masse"], vitesse=mercury["vitesse"], nom="Mercure"),
         sm1.corps(position=venus["position"], masse=venus["masse"], vitesse=venus["vitesse"], nom="Venus"),
         sm1.corps(position=earth["position"], masse=earth["masse"], vitesse=earth["vitesse"], nom="Earth"),
@@ -75,66 +74,66 @@ if __name__ == '__main__':
 
     # integration = inte.euler(corps_simulation_planete_9_min, pas_temps=7 * 86400)
     # mouvement_complet_avec_planete_9_min = sm1.run_simulation(integration,nombre_de_pas=1000*365/7,frequence=1)
+    # with open('mouvement_complet_avec_planete_9_min.p', 'wb') as fp:
+    #     pickle.dump(mouvement_complet_avec_planete_9_min, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
     # sm1.Graphique_plusieurs_corps(mouvement_complet_avec_planete_9_min[0],"Simulation du système solaire (Planètes Géantes) \n avec une planète 9",8, 11, outfile=None)
 
-    "Simulation planete X(9) max "
+    nom_fichier = 'horizons_results-3.txt'
+    nom_planete = 'Réference'
+    mouvement_ref = extraction_donnees.extraire_donne(nom_fichier, nom_planete)
 
-    corps_simulation_planete_9_max = [
-        sm1.corps(position=sun["position"], masse=sun["masse"], vitesse=sun["vitesse"], nom="Sun"),
-        sm1.corps(position=mercury["position"], masse=mercury["masse"], vitesse=mercury["vitesse"], nom="Mercure"),
-        sm1.corps(position=venus["position"], masse=venus["masse"], vitesse=venus["vitesse"], nom="Venus"),
-        sm1.corps(position=earth["position"], masse=earth["masse"], vitesse=earth["vitesse"], nom="Earth"),
-        sm1.corps(position=mars["position"], masse=mars["masse"], vitesse=mars["vitesse"], nom="Mars"),
-        sm1.corps(position=jupiter["position"], masse=jupiter["masse"], vitesse=jupiter["vitesse"], nom="Jupiter"),
-        sm1.corps(position=saturn["position"], masse=saturn["masse"], vitesse=saturn["vitesse"], nom="Saturn"),
-        sm1.corps(position=uranus["position"], masse=uranus["masse"], vitesse=uranus["vitesse"], nom="Uranus "),
-        sm1.corps(position=neptune["position"], masse=neptune["masse"], vitesse=neptune["vitesse"], nom="Neptune "),
-        sm1.corps(position=pluto["position"], masse=pluto["masse"], vitesse=pluto["vitesse"], nom="Pluton"),
-        sm1.corps(position=planete_9_max["position"], masse=planete_9_max["masse"], vitesse=planete_9_max["vitesse"],
-                  nom=" Planète 9 ")]
-
-    integration = inte.euler(corps_simulation_planete_9_max, pas_temps=7 * 86400)
-    mouvement_complet_avec_planete_9_max = sm1.run_simulation(integration, nombre_de_pas=1000 * 365 / 7, frequence=1)
-
-    # nom_fichier = 'horizons_results-3.txt'
-    # nom_planete = 'Réference'
-    # mouvement_ref = extraction_donnees.extraire_donne(nom_fichier, nom_planete)
     with open('mouvement_avec_neptune.p', 'rb') as fp:
-        mouvement_ref = pickle.load(fp)
+        mouvement_avec_neptune = pickle.load(fp)
+    with open('mouvement_complet_avec_planete_9_min.p', 'rb') as fp:
+        mouvement_complet_avec_planete_9_min = pickle.load(fp)
+
+    """ comparaison orbite 9 et reference"""
+    mouvement_complet = mouvement_complet_avec_planete_9_min[0]
+    mouvement_complet[7]['nom'] = 'Simulation\n avec planète 9 '
+    mouvement_ref.append(mouvement_complet)
+    # Graph_2.graph2d_ref(mouvement_ref,
+    #                     "Graphique de comparaison entre la référence \n de l'orbite d'Uranus avec l'influence de la planète 9 ",
+    #                     outfile=None)
+    rayon_avec_9 = np.sqrt((np.array(mouvement_complet_avec_planete_9_min[0][7]['y'][0:101]) - np.array(mouvement_complet_avec_planete_9_min[0][0]['y'][0:101])) ** 2 + (
+                                 np.array(mouvement_complet_avec_planete_9_min[0][7]['x'][0:101]) - np.array(
+                             mouvement_complet_avec_planete_9_min[0][0]['x'][0:101])) ** 2)
+    rayon_ref = np.sqrt(np.array(mouvement_ref[0]['y']) ** 2 + np.array(mouvement_ref[0]['x']) ** 2)
+    print(f'rayon de la réference :{rayon_ref.mean():.5e}')
+    print(f'rayon avec planète 9 :{rayon_avec_9.mean():.5e}')
+    print(f'La différence entre les deux rayons est de: {(abs(rayon_ref.mean() - rayon_avec_9.mean())):.5e}  ')
+
+    print(f'Le pourcentage de différence entre les deux rayons est de: {(1 - abs((rayon_avec_9.mean())) / rayon_ref.mean()) * 100:.5} % ')
+
     # sm1.Graphique_plusieurs_corps(mouvement_complet_avec_planete_9_max[0],"Simulation du système solaire (Planètes Géantes) \n avec une planète 9", 8, 11,outfile=None)
-    rayon_avec = np.sqrt((np.array(mouvement_complet_avec_planete_9_max[0][7]['y'][0:101]) - np.array(
-        mouvement_complet_avec_planete_9_max[0][0]['y'][0:101])) ** 2 + (
-                                     np.array(mouvement_complet_avec_planete_9_max[0][7]['x'][0:101]) - np.array(
-                                 mouvement_complet_avec_planete_9_max[0][0]['x'][0:101])) ** 2)
-    rayon_ref = np.sqrt((np.array(mouvement_ref[0][7]['y'])-np.array(mouvement_ref[0][0]['y'])) ** 2 + (np.array(mouvement_ref[0][7]['x'])-np.array(mouvement_ref[0][0]['x'])) ** 2)
-    print(f'{rayon_ref.mean():.5e}')
-    print(f'{rayon_avec.mean():.5e}')
-    print(
-        f'La différence entre les deux rayons est de: {(abs(rayon_ref.mean() - rayon_avec.mean())):.5e}  ')
 
-    print(
-        f'Le pourcentage de différence entre les deux rayons est de: {(1 - abs((rayon_avec.mean())) / rayon_ref.mean()) * 100:.5} % ')
+    # """ comparaison orbite 9 et simulation avec Neptune"""
 
-    "Simulation Rogue 1 Masse:2xjupitere"
+    with open('mouvement_complet_avec_planete_9_min.p', 'rb') as fp:
+        mouvement_complet_avec_planete_9_min = pickle.load(fp)
+    with open('mouvement_avec_neptune.p', 'rb') as fp:
+        mouvement_avec_neptune_2 = pickle.load(fp)
 
 
-    corps_simulation_rogue_1 = [
-        sm1.corps(position=sun["position"], masse=sun["masse"], vitesse=sun["vitesse"], nom="Sun"),
-        sm1.corps(position=mercury["position"], masse=mercury["masse"], vitesse=mercury["vitesse"], nom="Mercure"),
-        sm1.corps(position=venus["position"], masse=venus["masse"], vitesse=venus["vitesse"], nom="Venus"),
-        sm1.corps(position=earth["position"], masse=earth["masse"], vitesse=earth["vitesse"], nom="Earth"),
-        sm1.corps(position=mars["position"], masse=mars["masse"], vitesse=mars["vitesse"], nom="Mars"),
-        sm1.corps(position=jupiter["position"], masse=jupiter["masse"], vitesse=jupiter["vitesse"], nom="Jupiter"),
-        sm1.corps(position=saturn["position"], masse=saturn["masse"], vitesse=saturn["vitesse"], nom="Saturn"),
-        sm1.corps(position=uranus["position"], masse=uranus["masse"], vitesse=uranus["vitesse"], nom="Uranus "),
-        sm1.corps(position=neptune["position"], masse=neptune["masse"], vitesse=neptune["vitesse"], nom="Neptune "),
-        sm1.corps(position=pluto["position"], masse=pluto["masse"], vitesse=pluto["vitesse"], nom="Pluton"),
-        sm1.corps(position=rogue_1["position"], masse=rogue_1["masse"], vitesse=rogue_1["vitesse"],
-                  nom=" Première Rogue ")]
-    # integration = inte.euler(corps_simulation_rogue_1, pas_temps=2 * 86400)
-    # mouvement_complet_avec_rogue_1 = sm1.run_simulation(integration,nombre_de_pas=10*365/2,frequence=1)
-    #
-    # sm1.Graphique_plusieurs_corps_sans_mvt_sol(mouvement_complet_avec_rogue_1[0],
-    #                               "Simulation de l'orbite des planètes géantes \n avec une planète vagabonde",
-    #                               0, 11, outfile=None)
+    mouvement_complet_avec_9 = mouvement_complet_avec_planete_9_min[0]
+    rayon_avec = np.sqrt((np.array(mouvement_complet_avec_9[7]['y']) - np.array(mouvement_complet_avec_9[0]['y'])) ** 2 + (
+            np.array(mouvement_complet_avec_9[7]['x']) - np.array(
+        mouvement_complet_avec_9[0]['x'])) ** 2)
+    rayon_ref_2 = np.sqrt((np.array(mouvement_avec_neptune_2[0][7]['y']) - np.array(mouvement_avec_neptune_2[0][0]['y'])) ** 2 + (
+            np.array(mouvement_avec_neptune_2[0][7]['x']) - np.array(mouvement_avec_neptune_2[0][0]['x'])) ** 2)
+    print(f'rayon sans planète 9 :{rayon_ref_2.mean():.5e}')
+    print(f'rayon avec planète 9 :{rayon_avec.mean():.5e}')
+    print(f'La différence entre les deux rayons est de: {(abs(rayon_ref_2.mean() - rayon_avec.mean())):.5e}  ')
+
+    print(f'Le pourcentage de différence entre les deux rayons est de: {(1 - abs((rayon_avec.mean())) / rayon_ref_2.mean()) * 100:.5} % ')
+
+    mouvement_pour_graph = []
+    mvt=[]
+    mouvement_complet_sans_9 = mouvement_avec_neptune_2[0]
+    mouvement_complet_avec_9[7]['nom'] = 'Simulation\n avec planète 9 '
+    mouvement_complet_sans_9[7]['nom'] = 'Simulation\n sans planète 9'
+    mouvement_pour_graph.append([mouvement_complet_sans_9[0], mouvement_complet_sans_9[7]])
+    mouvement_pour_graph.append([mouvement_complet_avec_9[0],mouvement_complet_avec_9[7]])
+
+    # Graph_2.graph2d_compa(mouvement_pour_graph, "Graphique de comparaison avec et sans l'influence de la planète 9 \n sur l'orbite d'Uranus",outfile=None)
+#
